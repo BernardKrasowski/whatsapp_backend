64bbad7b7e3d5c3fae116d5d3802ace5a6303260
Bernard <bernardkrasowski111@gmail.com>
2021-07-27 20:40:40 +0100

    push on gitHub

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import Messages from './dbMessages.js'
 import Pusher from 'pusher'
-
+import cors from 'cors'
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -22,7 +22,9 @@ const pusher = new Pusher({
 
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
 //DB config
 const password = 'zmpRTMfAt9FGT1MG';
 const connection_url = `mongodb+srv://admin:${password}@cluster0.d1y1e.mongodb.net/mywhatsapp?retryWrites=true&w=majority`;
@@ -33,9 +35,30 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true
 });
 
+// change streem: 
 
-//????
+mongoose.connection.once("open", () => {
+  console.log('db connected')
+  mongoose.connection.collection("messagecontents").watch().on("change", (change) => {
 
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted',
+
+        {
+          name: messageDetails.name,
+          message: messageDetails.message,
+          timestamp: messageDetails.timestamp,
+          received: messageDetails.received
+        }
+      );
+    } else {
+
+      console.log('Error triggering Pusher')
+    }
+
+  })
+})
 
 // api routes
 app.get('/', (req, res) => res.status(200).send('hellow world'))
